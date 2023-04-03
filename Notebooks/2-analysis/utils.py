564e0ddc34ab50988e4ee463e8a077bdbea0cc7d
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2023-04-03 19:34:09 +0200

    Added partial correlations, graphs and continued with data analysis

--- Notebooks/2-analysis/utils.py
+++ Notebooks/2-analysis/utils.py
@@ -1,4 +1,4 @@
-import pandas as pd, scipy.stats
+import pandas as pd, scipy.stats, matplotlib.pyplot as plt
 
 
 def get_normalized_variation(s_df, sentinel):
@@ -12,7 +12,7 @@ def get_normalized_variation(s_df, sentinel):
     was higher then 0, considering different manure dates on the crop fields. 
 
     Parameters:
-        s_df (pandas DataFrame): A data object with filtered and pre-processed Sentinel-1 and Sentinel-2 images
+        s_df (pandas DataFrame): A data object containing sentinel-1 or sentinel-2 features extracted
         sentinel (int): The sentinel number (1 for Sentinel-1 or 2 for Sentinel-2).
         
     Returns:
@@ -30,14 +30,16 @@ def get_normalized_variation(s_df, sentinel):
         # Convert manure_dates column to a list of datetimes
         manure_dates = pd.to_datetime(s_df['manure_dates'].apply(lambda x: x.replace("['", "").replace("']", "")), format='%Y-%m-%d')
         
-        # Get the indices of the rows that are more between 0 and 30 days after the manure date
+        # Get the indices of the rows that are between 0 and 30 days after the manure date (why 30 days? Because the effects
+        # of fertilization on crop field can be seen not just the day immediately after, but also for few weeks after)
+        # https://www.mdpi.com/2072-4292/13/9/1616
         manure_indices = (s_df['crop_field_name'] == field_name) & ((pd.to_datetime(s_df['s' + str(sentinel) + '_acquisition_date'], format='%Y-%m-%d') - manure_dates[0]).dt.days <= 30) & ((pd.to_datetime(s_df['s' + str(sentinel) + '_acquisition_date'], format='%Y-%m-%d') - manure_dates[0]).dt.days >= 0)
         
         # Calculate the mean of the valid rows
-        mean_df = s_df[~manure_indices].select_dtypes(include=['float', 'int']).mean()
+        mean_df = s_df[~manure_indices].select_dtypes(include=['number']).mean()
         
         # Calculate the normalized difference between the mean and the first values of the manure_indices
-        norm_var_df = pd.DataFrame(s_df[manure_indices].select_dtypes(include=['float', 'int']).sub(mean_df).div(mean_df).iloc[0].rename('normalized_variation'))
+        norm_var_df = pd.DataFrame(s_df[manure_indices].select_dtypes(include=['number']).sub(mean_df).div(mean_df).iloc[0].rename('normalized_variation'))
         
         # Calculate the absolute normalized difference and sort by descending order
         abs_norm_var_df = norm_var_df.abs().sort_values(by=norm_var_df.columns[0], ascending=False).reset_index().rename(columns={"index": "feature"})
@@ -72,3 +74,42 @@ def get_normalized_variation(s_df, sentinel):
 
     # Return the DataFrame
     return results_df
+
+
+def display_features_trends(s_df, fields_pos, features, features_pos, sentinel):
+    '''
+    This function plots the trends of selected features for selected crop fields
+
+    Parameters:
+        s_df (pandas DataFrame): A DataFrame containing Sentinel-1 or Sentinel-2 features extracted.
+        fields_pos (tuple): A tuple containing the start and end position of the crop field names to plot.
+        features (list): A list of feature names to plot.
+        features_pos (tuple): A tuple containing the start and end position of the features to plot.
+        sentinel (int): The Sentinel number (1 for Sentinel-1 or 2 for Sentinel-2).
+    '''
+    # Iterate over each crop field name
+    for crop_field_name in s_df['crop_field_name'].unique()[fields_pos[0]: fields_pos[-1]]:
+        # Create a new plot
+        plt.figure()
+        legend = []
+        
+        # Iterate over each feature to plot
+        for feature in features[features_pos[0]: features_pos[-1]]:
+            # Plot the feature values for the crop field
+            plt.plot(s_df[s_df['crop_field_name'] == crop_field_name]['s' + str(sentinel) +'_acquisition_date'], 
+                     s_df[s_df['crop_field_name'] == crop_field_name][feature])
+            legend.append(feature)
+        
+        # Add a legend to the plot
+        plt.legend(legend)
+        
+        # Get the manure application dates for the crop field
+        manure_date = s_df[s_df['crop_field_name'] == crop_field_name]['manure_dates'].unique()
+        
+        # Set the plot title, x-axis label, and y-axis label
+        plt.title('Trend of features more impacted by manure\nCrop field name: ' + str(crop_field_name) + ' - Manure dates: ' + str(manure_date))
+        plt.xlabel('Acquisition date')
+        plt.ylabel('Feature value')
+        
+        # Rotate the x-axis ticks
+        plt.xticks(rotation=90)
